00d0677a13da68e040c76fb165e692c6df1d3739
MrRoboot0 <30445751+MrRoboot0@users.noreply.github.com>
2021-06-10 10:34:29 -0500

    UI
    Interface

---
+++ ColosseumGame/Assets/Scenes/MainMenu2.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class MainMenu2 : MonoBehaviour
+{
+    
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("ColosseumScene");
+    }
+
+    public void Atras()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    
+
+}
